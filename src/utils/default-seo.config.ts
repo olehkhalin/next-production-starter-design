65761b669c5223fa0637a1e831b386c423f163cb
olehkhalin <olehkhalin@gmail.com>
2021-03-14 15:34:41 +0200

    Changed website url

--- src/utils/default-seo.config.ts
+++ src/utils/default-seo.config.ts
@@ -4,7 +4,7 @@ export const DEFAULT_SEO = {
   DESCRIPTION_ZH: '我们如何将Next.js用于我们自己的项目以及客户的项目，以实现最佳用户实践和SEO优化。基于TypeScript，Linter＆Husky，组件级SASS，国际化（next-i18next），React SVG的生产就绪设置',
   DESCRIPTION_RU: 'Как мы используем Next.js в наших собственных и клиентских проектах для достижения лучших пользовательских практик и оптимизации SEO. Готовая к работе установка на основе TypeScript, Linter & Husky, SASS на уровне компонентов, интернационализация (next-i18next), React SVG.',
   SITE_NAME: 'Next Production Starter by @olehkhalin',
-  WEBSITE_URL: 'https://website-url/', // Slash in the end is necessary
+  WEBSITE_URL: 'https://next-production-starter.artkhalin.com/', // Slash in the end is necessary
   IMAGE: 'og_image.jpg',
   OG: {
     TYPE: 'website',
